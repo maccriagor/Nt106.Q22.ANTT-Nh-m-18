8933ea48dc9f17c60d0129649e8fb8a481b94873
agvmcne <hoangdung43q12@gmail.com>
2026-06-12 20:17:06 +0700

    merge update Khoa

--- source/CafeManagement/CafeServer/DatabaseService.cs
+++ source/CafeManagement/CafeServer/DatabaseService.cs
@@ -5,28 +5,43 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
+using Newtonsoft.Json.Linq;
 
 namespace CafeServer
 {
     public class DatabaseService
     {
-        private static string url = "https://alcocggifvzlrjdcvjeo.supabase.co";
-        private static string key = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFsY29jZ2dpZnZ6bHJqZGN2amVvIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTc3NDUxMTI5MywiZXhwIjoyMDkwMDg3MjkzfQ.JAFtQCMGmv4OKWy1PknhXwhnFfP-r5zszf_-vUsW5ow";
+        private static string url;
+        private static string key;
         private static Client _supabase;
         public static Client Client => _supabase;
 
         public static async Task InitializeAsync()
         {
             if (_supabase != null) return;
-            var options = new SupabaseOptions { AutoConnectRealtime = true };
-            _supabase = new Client(url, key, options);
-            await _supabase.InitializeAsync();
 
-            System.Console.WriteLine("--- Kết nối Supabase thành công! ---");
+            try
+            {
+                // 1. Đọc nội dung file appsettings.json
+                string jsonConfig = File.ReadAllText("appsettings.json");
+
+                // 2. Parse JSON để lấy giá trị ra
+                JObject config = JObject.Parse(jsonConfig);
+                url = (string)config["SupabaseSettings"]["Url"];
+                key = (string)config["SupabaseSettings"]["ServiceRoleKey"];
+
+                // 3. Khởi tạo Supabase bằng Key lấy từ file
+                var options = new SupabaseOptions { AutoConnectRealtime = true };
+                _supabase = new Client(url, key, options);
+                await _supabase.InitializeAsync();
+
+                Console.WriteLine("--- Kết nối Supabase thành công (Service Role Key)! ---");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"[LỖI KẾT NỐI DB] Không thể đọc cấu hình hoặc khởi tạo Supabase: {ex.Message}");
+            }
         }
-
-        
-
-        
     }
 }
